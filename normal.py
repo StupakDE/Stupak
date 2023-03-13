9b862dfc00f1ddef4036d214acf4042ace1da0d0
Дима Ступак <stupak.de@phystech.edu>
2023-03-13 19:03:22 +0300

    add show and gitignore

--- normal.py
+++ normal.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import show
 
 
 size = 1000
@@ -7,5 +7,4 @@ size = 1000
 x = np.linspace(0, size, size)
 y = np.random.normal(0, 1, size)
 
-plt.plot(x, y)
-plt.show()
+show.mp(x, y)
